33231e8972ff35a45b2ec7683a5fefc463aec987
david11yf29 <david11yf29@gmail.com>
2021-01-18 14:18:33 +0800

    updated

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,19 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="header">
+      <h1>This is the header</h1>
+
+      <div className="header__left">
+        <img src="" alt="" />
+        
+        <div className="header__search">
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+
+      </div>
 
     </div>
   )
