14bc42e48424c504fb20e047410700df3c9ebf01
david11yf29 <david11yf29@gmail.com>
2021-01-18 20:58:45 +0800

    updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,8 +13,23 @@ const Sidebar = () => {
         <h4>david11yf29@gmail.com</h4>
       </div>
 
-      <div className="sidebar__state">
-        
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">
+            2,543
+          </p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">
+            2,448
+          </p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
       </div>
     </div>
   )
