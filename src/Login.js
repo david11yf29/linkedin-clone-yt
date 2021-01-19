0499f7b7d2935e230bd3a17910cff8323e281bd3
david11yf29 <david11yf29@gmail.com>
2021-01-20 00:51:11 +0800

    updated

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
+    </div>
+  )
+}
+
+export default Login;
