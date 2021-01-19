83a9854a9701b66473c540e60e374dee4f97018b
david11yf29 <david11yf29@gmail.com>
2021-01-19 23:08:19 +0800

    updated

--- src/Feed.js
+++ src/Feed.js
@@ -18,7 +18,7 @@ const Feed = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setPosts(snapshot.docs.map((doc) => {
         return {
           id: doc.id,
