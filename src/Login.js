fcbce75ab29e632e7433843bd30559394072cd98
david11yf29 <david11yf29@gmail.com>
2021-01-20 18:32:57 +0800

    updated

--- src/Login.js
+++ src/Login.js
@@ -8,8 +8,15 @@ const Login = () => {
 
       <form>
         <input placeholder="Full name (required if registering)" type="text" />
-        <input  type="text" />
+        <input placeholder="Profile pic URL (optional)" type="text" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button>Sign In</button>
       </form>
+      <p>
+        Not a member?
+        <span className="">Register Now</span>
+      </p>
     </div>
   )
 }
