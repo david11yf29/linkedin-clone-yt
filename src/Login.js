d3c6f63bd7905a2293e267c7d4b8141041c8759f
david11yf29 <david11yf29@gmail.com>
2021-01-20 18:52:02 +0800

    updated

--- src/Login.js
+++ src/Login.js
@@ -22,10 +22,22 @@ const Login = () => {
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
 
       <form>
-        <input placeholder="Full name (required if registering)" type="text" />
+        <input 
+          placeholder="Full name (required if registering)" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          type="text" />
         <input placeholder="Profile pic URL (optional)" type="text" />
-        <input placeholder="Email" type="email" />
-        <input placeholder="Password" type="password" />
+        <input 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          type="email" />
+        <input 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          type="password" />
         <button type="submit" onClick={loginToApp}>Sign In</button>
       </form>
       <p>
