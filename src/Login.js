a93c3efb32b88b771e7881aa3221828d5e8f301a
david11yf29 <david11yf29@gmail.com>
2021-01-20 18:03:33 +0800

    updated

--- src/Login.js
+++ src/Login.js
@@ -5,6 +5,10 @@ const Login = () => {
   return (
     <div className="login">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
+
+      <form>
+        <input placeholder="Full name (required if registering)" type="text" />
+      </form>
     </div>
   )
 }
