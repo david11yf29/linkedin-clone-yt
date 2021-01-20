39d3346ba2f5d46e8aa1ddae1d9a215ddb3f6cd2
david11yf29 <david11yf29@gmail.com>
2021-01-20 18:35:43 +0800

    updated

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './Login.css';
 
 const Login = () => {
+
+  const loginToApp = () => {
+    
+  };
+
+  const register = () => {
+
+  };
+
   return (
     <div className="login">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
@@ -11,11 +20,11 @@ const Login = () => {
         <input placeholder="Profile pic URL (optional)" type="text" />
         <input placeholder="Email" type="email" />
         <input placeholder="Password" type="password" />
-        <button>Sign In</button>
+        <button type="submit" onClick={loginToApp}>Sign In</button>
       </form>
       <p>
         Not a member?
-        <span className="">Register Now</span>
+        <span className="login__register" onClick={register} >Register Now</span>
       </p>
     </div>
   )
