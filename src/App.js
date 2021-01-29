4944ef9923f1e967f3ea9431357c715067c07723
david11yf29 <david11yf29@gmail.com>
2021-01-30 00:20:53 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Feed from './Feed';
-import Login from './Login';
-import Widgets from './Widgets';
-import { login, logout, selectUser } from './features/userSlice';
-import { auth } from './firebase';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import Login from "./Login";
+import Widgets from "./Widgets";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
-function App() {
-
+const App = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    auth.onAuthStateChanged(userAuth => {
+    auth.onAuthStateChanged((userAuth) => {
       // user is logged in
       if (userAuth) {
-        dispatch(login({
-          email: userAuth.email,
-          uid: userAuth.uid,
-          displayName: userAuth.displayName,
-          photoUrl: userAuth.photoURL
-        }))
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
       } else {
-        // user is logged out 
-        dispatch(logout())
+        // user is logged out
+        dispatch(logout());
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div className="app">
       {/* Header */}
       <Header />
 
-      { !user ? <Login /> : (
+      {!user ? (
+        <Login />
+      ) : (
         <div className="app__body">
           <Sidebar />
           <Feed />
           <Widgets />
         </div>
-      ) }
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,8 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App";
 
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// enzyme.configure({ adapter: new Adapter() });
+
 test("renders learn react link", () => {});
